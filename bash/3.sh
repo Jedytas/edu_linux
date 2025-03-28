2e042d2029fd000ccefd39675bd18a2917c70d5c
Stall Man <adbak3989@gmail.com>
2025-03-28 22:05:36 +0000

    miniup

--- bash/3.sh
+++ bash/3.sh
@@ -2,7 +2,7 @@
 
 for file in "$@"; do
     if test -f "$file"; then
-        strokes=$(wc -l < "$file")  # Используем редирект для подсчета строк
+        strokes=$(wc -l < "$file")
         echo "$file -> $strokes"
     else
         echo "Not found or not a file: $file"

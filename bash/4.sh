842b0a40955b646e46a7feb75abf4eb7a393f59e
Stall Man <adbak3989@gmail.com>
2025-05-16 23:14:42 +0000

    mini up

--- bash/4.sh
+++ bash/4.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-pl=$1
 
-case pl in
-    "Mercury") echo "0" ;;
-    "Venus") echo "0" ;;
-    "Earth") echo "1" ;;
-    "Mars") echo "2" ;;
-    "Jupiter") echo "98" ;;
-    "Saturn") echo "83" ;;
-    "Uranus") echo "27" ;;
-    "Neptune") echo "14" ;;
+
+case $1 in
+    "Mercury") echo 0 ;;
+    "Venus") echo 0 ;;
+    "Earth") echo 1 ;;
+    "Mars") echo 2 ;;
+    "Jupiter") echo 98 ;;
+    "Saturn") echo 83 ;;
+    "Uranus") echo 27 ;;
+    "Neptune") echo 14 ;;
     *) echo "Unknown planet"
         exit 1
         ;;
 esac
+
